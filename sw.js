8dfeb31f4eed41727eb6455634e27a3ee2f5b335
JapodianMak <85761202+JapodianMak@users.noreply.github.com>
2026-08-31 18:00:05 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE = "makan-orodje-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./html5-qrcode.min.js",
+  "./qrcode.min.js",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then((hit) => {
+      return (
+        hit ||
+        fetch(e.request)
+          .then((res) => {
+            const copy = res.clone();
+            caches.open(CACHE).then((c) => c.put(e.request, copy));
+            return res;
+          })
+          .catch(() => caches.match("./index.html"))
+      );
+    })
+  );
+});
